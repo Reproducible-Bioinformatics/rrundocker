eb154112ea6b82bde3c6eac2bd0dc06670b5a6ac
Eliseo Martelli <me@eliseomartelli.it>
2024-07-29 14:09:50 +0200

    feat: add is_running_in_docker function

--- R/run_in_docker.R
+++ R/run_in_docker.R
@@ -2,7 +2,7 @@
 #'
 #' @param image_name The docker image you want to run.
 #' @param volumes The list of volumes to mount to the container.
-#' @additional_arguments Vector of arguments to pass to the container.
+#' @param additional_arguments Vector of arguments to pass to the container.
 #' @export
 run_in_docker <- function(image_name,
                           volumes = list(),
@@ -23,4 +23,20 @@ run_in_docker <- function(image_name,
     base_command <- paste(base_command, argument)
   }
   system2("docker", args = base_command, stdout = TRUE, stderr = TRUE)
+}
+
+#' Check if the script is running in a container.
+#'
+#' @returns A truthy value indicating the state.
+#' @export
+is_running_in_docker <- function() {
+  dockerenv_exists <- file.exists("/.dockerenv")
+  cgroup_exists <- file.exists("/proc/1/cgroup")
+  in_container_runtime <- FALSE
+  if (cgroup_exists) {
+    in_container_runtime <- any(
+      grepl("docker", readLines("/proc/1/cgroup", warn = FALSE))
+    )
+  }
+  return(dockerenv_exists || in_container_runtime)
 }
